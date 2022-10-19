10a7aaacf0f22871c8e4c211200fb77a9c191964
Ian Engelbrecht <ianicus.za@gmail.com>
2022-10-19 14:09:58 +0200

    edits to movecollectionobjects

--- moveCollectionObjects/index.js
+++ moveCollectionObjects/index.js
@@ -107,7 +107,11 @@ bar.start(collectionObjects.length, 0)
 
 //run the updates as a transaction
 await query('START TRANSACTION')
-let counter = 0
+let coUpdateCounter = 0
+let coaUpdateCounter = 0
+let prepUpdateCounter = 0
+let prepAttributeUpdateCounter = 0
+let detUpdateCounter = 0
 for (const co of collectionObjects) {
   
   // get the determinations and update
@@ -117,8 +121,10 @@ for (const co of collectionObjects) {
     const updateDetSQL = `UPDATE determination SET collectionmemberid = ${destCollection.UserGroupScopeId} WHERE determinationid = ${det.DeterminationID}`
     try {
       await query(updateDetSQL)
+      detUpdateCounter++
     }
     catch(err) {
+      bar.stop()
       console.log('There was an error updating dets:')
       console.error(err)
       console.log('executing rollback...')
@@ -146,12 +152,51 @@ for (const co of collectionObjects) {
     }
     const newPrepTypeID = destPrepTypes[prepType]
 
+
+    //update prep attributes
+    if (prep.PreparationAttributeID != null) {
+      const updatePrepAttributeSQL = `UPDATE preparationattribute SET collectionmemberid = ${destCollection.UserGroupScopeId} WHERE preparationattributeid = ${prep.PreparationAttributeID}`
+      try {
+        await query(updatePrepAttributeSQL)
+        prepAttributeUpdateCounter++
+      }
+      catch(err) {
+        bar.stop()
+        console.log('There was an error updating prep attribute:')
+        console.error(err)
+        console.log('executing rollback...')
+        await query('ROLLBACK')
+        console.log('please fix the error and try again')
+        process.exit()
+      }
+    }
+
     const updatePrepSQL = `UPDATE preparation SET collectionmemberid = ${destCollection.UserGroupScopeId}, preptypeid = ${newPrepTypeID} WHERE preparationid = ${prep.PreparationID}`
     try {
       await query(updatePrepSQL)
+      prepUpdateCounter++
     }
     catch(err) {
+      bar.stop()
       console.log('There was an error updating preps:')
+      console.error(err)
+      console.log('executing rollback...')
+      await query('ROLLBACK')
+      console.log('please fix the error and try again')
+      process.exit()
+    }
+  }
+  
+  //update any collectionobjectattributes
+  if(co.CollectionObjectAttributeID != null) {
+    const updateCollectionObjectAttributeSQL = `UPDATE collectionobjectattribute SET collectionmemberid = ${destCollection.UserGroupScopeId} WHERE collectionobjectattributeid = ${co.CollectionObjectAttributeID}`
+    try {
+      await query(updateCollectionObjectAttributeSQL)
+      coaUpdateCounter++
+    }
+    catch(err) {
+      bar.stop()
+      console.log('There was an error updating collectionobjectattribute:')
       console.error(err)
       console.log('executing rollback...')
       await query('ROLLBACK')
@@ -165,8 +210,8 @@ for (const co of collectionObjects) {
     WHERE collectionobjectid = ${co.CollectionObjectID}`
   try {
     await query(updateCollectionObjectSQL)
-    counter++
-    bar.update(counter)
+    coUpdateCounter++
+    bar.update(coUpdateCounter)
   }
   catch(err) {
     bar.stop()
@@ -179,8 +224,15 @@ for (const co of collectionObjects) {
   }
 }
 
+bar.stop()
 console.log('all changes applies successfully, committing...')
 await query('COMMIT')
 conn.destroy()
+
+console.log('Updated', coUpdateCounter, 'collection objects, with...')
+console.log(detUpdateCounter, 'determinations...')
+console.log(prepUpdateCounter, 'preparations...')
+console.log(prepAttributeUpdateCounter, 'preparation attribute records, and...')
+console.log(coaUpdateCounter, 'collection object attribute records')
 
 console.log('all done...')
